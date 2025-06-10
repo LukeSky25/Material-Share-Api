4e5c6f5b2c539b65537570c1139c68d87e1ba494
Lucas Lucena Rodrigues dos Santos <rm89997@estudante.fieb.edu.br>
2025-06-10 12:24:55 -0300

    first commit

---
+++ src/main/java/br/itb/projeto/material_share/service/CategoriaService.java
@@ -0,0 +1,42 @@
+package br.itb.projeto.material_share.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import br.itb.projeto.material_share.model.entity.Categoria;
+import br.itb.projeto.material_share.model.repository.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	private CategoriaRepository categoriaRepository;
+
+	public CategoriaService(CategoriaRepository categoriaRepository) {
+		super();
+		this.categoriaRepository = categoriaRepository;
+	}
+	
+	public Categoria findById(long id) {
+
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+
+		if (categoria.isPresent()) {
+			return categoria.get();
+		}
+
+		return null;
+	}
+	
+	public List<Categoria> findAll(){
+		List<Categoria> categorias = categoriaRepository.findAll();
+	
+		return categorias;
+	}
+}
+
+
+
+
+
