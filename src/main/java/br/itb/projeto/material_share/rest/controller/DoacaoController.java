720b85412f0c20b654922f290cf41552585bd4e8
Lucas Lucena Rodrigues dos Santos <rm89997@estudante.fieb.edu.br>
2025-07-08 08:53:13 -0300

    Resolução de problemas relacioandos a doação

--- src/main/java/br/itb/projeto/material_share/rest/controller/DoacaoController.java
+++ src/main/java/br/itb/projeto/material_share/rest/controller/DoacaoController.java
@@ -1,14 +1,17 @@
 package br.itb.projeto.material_share.rest.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.itb.projeto.material_share.model.entity.Doacao;
-import br.itb.projeto.material_share.model.repository.DoacaoRepository;
 import br.itb.projeto.material_share.rest.response.MessageResponse;
 import br.itb.projeto.material_share.service.DoacaoService;
 
@@ -18,22 +21,20 @@ public class DoacaoController {
 
     private final DoacaoService doacaoService;
 
-	
-
-	private DoacaoRepository doacaoRepository;
-	
 	public DoacaoController(DoacaoService doacaoService) {
 		super();
 		this.doacaoService = doacaoService;
 	}
 	
-	@GetMapping("/test")
-	public String getTest() {
-		return "Olá, Doacao!";
+	@GetMapping("/findAll")
+	public ResponseEntity<List<Doacao>> findAll() {
+		List<Doacao> doacoes = doacaoService.findAll();
+		
+		return new ResponseEntity<List<Doacao>>(doacoes, HttpStatus.OK);
 	}
 	
-	@PostMapping("/createSemFoto")
-	public ResponseEntity<?> create(@ModelAttribute Doacao doacao) {
+	@PostMapping("/createComFoto")
+	public ResponseEntity<?> createComFoto(@ModelAttribute Doacao doacao) {
 		
 		Doacao _doacao = doacaoService.create(doacao);
 		
@@ -43,6 +44,19 @@ public class DoacaoController {
 		
 		return ResponseEntity.ok().body(new MessageResponse("Doacao cadastrada com sucesso!"));
 	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<?> create(@RequestBody Doacao doacao) {
+		
+		Doacao _doacao = doacaoService.create(doacao);
+		
+		if(_doacao == null) {
+			return ResponseEntity.badRequest().body(new MessageResponse("Doacao já cadastrada!"));
+		}
+		
+		return ResponseEntity.ok().body(new MessageResponse("Doacao cadastrada com sucesso!"));
+	}
+	
 	
 	
 
